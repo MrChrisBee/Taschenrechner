711d0888082de6c548d63cfdefc27caed274746b
MrChrisBee <chris@cokuss.de>
2022-06-07 10:27:57 +0200

    ltns

--- CalculateTK.py
+++ CalculateTK.py
@@ -1,39 +1,47 @@
 from tkinter import *
+from typing import List, Any
 
 from numpy import column_stack
 
-#1 create window instance
+
+# 1 create window instance
+
+
+def add_str(what: str):
+    pass
+
 
 window = Tk()
 window.title("TK-Calc")
-window.geometry("600x600") # with x height
+#  window.geometry("600x600")  # with x height
 window.config(
     background="#00f"
 )
-_tasten = ['1','2','3','+','4','5','6','-','7','8','9','*','.','0','=','/']
-_actions = ['add(1)','add(2)','add(3)','add(+)','add(4)','add(5)','add(6)','add(-)','add(7)',
-            'add(8)','add(9)','add(*)','add(.)','add(0)','calculate()', 'add(/)']
 
-for taste, action in zip(_tasten,_actions):
+butt: list[str | Any] = ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '.', '0', '=', '/']
+actions: list[str | Any] = ['add(1)', 'add(2)', 'add(3)', 'add(+)', 'add(4)', 'add(5)', 'add(6)', 'add(-)', 'add(7)',
+                            'add(8)', 'add(9)', 'add(*)', 'add(.)', 'add(0)', 'calculate()', 'add(/)']
+
+for taste, action in zip(butt, actions):
     print(taste, action)
 
 
 def digits_label():
-    
     # Define the label
-    label = Label(window, 
-        text = "0", 
-        font=("Ink Free", 20, "bold"),
-        height=1,
-        width=20,
-        anchor=E,
-        padx = 10,  # Padding x
-        pady = 10,  # Padding y
-        
-        bg = "red",  # Background Color
-        fg = "blue"  # Foreground Color --> Font Color
-    )
-    #label.pack(pady=20)
-    label.grid(row=0,column=0,columnspan=4,sticky=N)
+    label = Label(window,
+                  text="0",
+                  font=("Ink Free", 20, "bold"),
+                  height=1,
+                  width=20,
+                  anchor=E,
+                  padx=10,  # Padding x
+                  pady=10,  # Padding y
+                  bg="red",  # Background Color
+                  fg="blue"  # Foreground Color --> Font Color
+                  )
+    # label.pack(pady=20)
+    label.grid(row=0, column=0, columnspan=4, sticky=N)
+
+
 digits_label()
 window.mainloop()
